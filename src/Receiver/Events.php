2dd81371deb1aec24dd8633d055244efa043a823
David Sanchez <david38sanchez@gmail.com>
2018-08-23 19:36:29 -0400

    Add new method `tokenStatus` for class `Events`

--- src/Receiver/Events.php
+++ src/Receiver/Events.php
@@ -13,11 +13,26 @@ class Events
     use Receiver;
 
     /**
+     * Ping API.
+     *
      * @return array
      * @throws \Http\Client\Exception
      */
     public function ping(): array
     {
         return $this->getClient()->get('/ping');
+    }
+
+    /**
+     * Get status for the given token.
+     *
+     * @param string $token
+     *
+     * @return array
+     * @throws \Http\Client\Exception
+     */
+    public function tokenStatus(string $token): array
+    {
+        $this->getClient()->get('/token/status', ['token' => $token]);
     }
 }
