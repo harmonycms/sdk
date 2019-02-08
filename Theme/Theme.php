94436bcc37e6a5d17cdb029246ea29ded8aa2c32
David Sanchez <david.sanchez@nventive.com>
2019-02-08 13:09:21 -0500

    Add new default methods

--- Theme/Theme.php
+++ Theme/Theme.php
@@ -23,12 +23,52 @@ class Theme implements ThemeInterface
      */
     final public function getName(): string
     {
+        try {
+            $reflexionConstant = new \ReflectionClassConstant($this, 'NAME');
+            $this->name        = $reflexionConstant->getValue();
+        }
+        catch (\Exception $e) {
+        }
+
         if (null === $this->name) {
             $pos        = strrpos(static::class, '\\');
             $this->name = false === $pos ? static::class : substr(static::class, $pos + 1);
         }
 
         return $this->name;
+    }
+
+    /**
+     * Returns the theme description.
+     *
+     * @return string The Theme description
+     */
+    final public function getDescription(): string
+    {
+        try {
+            $reflexionConstant = new \ReflectionClassConstant($this, 'DESCRIPTION');
+
+            return $reflexionConstant->getValue();
+        }
+        catch (\Exception $e) {
+        }
+
+        return '';
+    }
+
+    /**
+     * Returns the theme preview image.
+     *
+     * @return string The theme preview image
+     */
+    public function getPreview(): string
+    {
+        $array = glob($this->getPath() . '/assets/images/preview.{jpg,jpeg,png,gif}', GLOB_BRACE);
+        if (isset($array[0])) {
+            return sprintf('/themes/%s/%s', '', (new \SplFileInfo($array[0]))->getBasename());
+        }
+
+        return '';
     }
 
     /**
--- Theme/ThemeInterface.php
+++ Theme/ThemeInterface.php
@@ -11,11 +11,25 @@ interface ThemeInterface
 {
 
     /**
-     * Returns the theme name (the class short name).
+     * Returns the theme name.
      *
      * @return string The Theme name
      */
     public function getName(): string;
+
+    /**
+     * Returns the theme description.
+     *
+     * @return string The Theme description
+     */
+    public function getDescription(): string;
+
+    /**
+     * Returns the theme preview image.
+     *
+     * @return string The theme preview image
+     */
+    public function getPreview(): string;
 
     /**
      * Gets the Theme directory path.
